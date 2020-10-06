32b84c599a563b1e2025dd7576b99c4782a23b25
hippwndon <haedge@asu.edu>
2020-10-05 21:59:24 -0700

    adding version one

---
+++ AddingMachine.java
@@ -0,0 +1,52 @@
+package cse360assignment02;
+
+/**
+ * @author      Hannah Edge <haedge@asu.edu>
+ */
+public class AddingMachine {
+  private int total;
+  
+  /**
+ * program that adds and subtracts
+ * constructor for AddingMachine
+ */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+ * obtains sum of values from add/subtract
+ * @return total
+ */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+ * adds value to sum, computes addition
+ * @param  value added (self explanatory)
+ */
+  public void add (int value) {
+  }
+
+/**
+ * subtracts value from sum 
+ * @param  value added
+ */
+  public void subtract (int value) {
+  }
+
+/**
+ * converts to string
+ * @return formatter (self explanatory)
+ */
+  public String toString () {
+    return "";
+  }
+
+/**
+ * clears memory
+ */
+  public void clear() {
+  }
+}
